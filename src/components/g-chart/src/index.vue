33695a6f4184281939deab4d9ccdcd24d4234dd6
yokiyokiyoki <419703726@qq.com>
2017-12-06 22:15:51 +0800

    图表组件初始化

--- src/components/g-chart/src/index.vue
+++ src/components/g-chart/src/index.vue
@@ -1,19 +1,138 @@
 <style lang="less" scoped>
   .chart-box {
-    height: 100%;
-    width: 100%;
+    height: 340px;
+    width: 100px;
   }
 </style>
 
 <template>
-  <div class="chart-box">11</div>
+    <div class="chart-box" ></div>
 </template>
 <script>
+  import ec from "echarts";
+  // enumerating ECharts events 枚举echarts事件
+  const ACTION_EVENTS = [
+    "legendselectchanged",
+    "legendselected",
+    "legendunselected",
+    "datazoom",
+    "datarangeselected",
+    "timelinechanged",
+    "timelineplaychanged",
+    "restore",
+    "dataviewchanged",
+    "magictypechanged",
+    "geoselectchanged",
+    "geoselected",
+    "geounselected",
+    "pieselectchanged",
+    "pieselected",
+    "pieunselected",
+    "mapselectchanged",
+    "mapselected",
+    "mapunselected",
+    "axisareaselected",
+    "brush",
+    "brushselected"
+  ];
+
+  const MOUSE_EVENTS = [
+    "click",
+    "dblclick",
+    "mouseover",
+    "mouseout",
+    "mousedown",
+    "mouseup",
+    "globalout"
+  ];
   export default {
     name: "g-chart",
     data() {
-      return {};
+      return {
+        instance: null, //echart实例
+        isEmptyData: false //是否是空数据
+      };
     },
-    methods: {}
+    mounted() {
+      this.init();
+    },
+    methods: {
+      init() {
+        this.$nextTick(() => {
+          document.addEventListener(window, "resize", this.resizeEventHandler);
+          this.initOption();
+        });
+      },
+      initOption() {
+        if (this.dataComplete) {
+          if (!isEmptyObject(this.options)) {
+            if (this.options.series && this.options.series.length != 0) {
+              this.instance = ec.init(this.$el, "default");
+              this.instance.setOption(this.options, true);
+              // expose ECharts events as custom events（遍历echart事件emit）
+
+              ACTION_EVENTS.forEach(event => {
+                this.instance.on(event, params => {
+                  this.$emit(event, params);
+                });
+              });
+              MOUSE_EVENTS.forEach(event => {
+                this.instance.on(event, params => {
+                  this.$emit(event, params);
+                });
+              });
+            } else {
+              this.isEmptyData = true;
+            }
+          } else {
+            this.isEmptyData = true;
+          }
+        }
+      },
+      resizeEventHandler() {
+        if (this.instance) {
+          throttle(this.instance.resize(), 300);
+        }
+      }
+    },
+    props: {
+      dataComplete: {
+        type: Boolean,
+        default() {
+          return false;
+        },
+        required: true
+      },
+      options: {
+        type: Object,
+        default() {
+          return {};
+        },
+        required: true
+      }
+    },
+    beforeDestroy() {
+      document.removeEventListener(window, "resize", this.resizeEventHandler);
+    }
   };
+
+  function isEmptyObject(obj) {
+    for (let x in obj) {
+      return false;
+    }
+    return true;
+  }
+
+  // 节流函数
+  function throttle(fn, delay) {
+    var timer = null;
+    return function() {
+      var context = this,
+        args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        fn.apply(context, args);
+      }, delay);
+    };
+  }
 </script>
--- src/page/Index/index.vue
+++ src/page/Index/index.vue
@@ -1,8 +1,66 @@
 <template>
     <div>
-      <g-chart></g-chart>
+      <g-chart :options="opt" :data-complete="dataComplete"></g-chart>
+      11
     </div>
 </template>
 <script>
-    export default {};
+    export default {
+      data() {
+        return {
+          opt: handleOpt(),
+          dataComplete: false
+        };
+      },
+      mounted() {
+        this.dataComplete = true;
+      }
+    };
+
+    function handleOpt() {
+      return {
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          x: "left",
+          data: ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"]
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: ["50%", "70%"],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: "center"
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: "30",
+                  fontWeight: "bold"
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              { value: 335, name: "直接访问" },
+              { value: 310, name: "邮件营销" },
+              { value: 234, name: "联盟广告" },
+              { value: 135, name: "视频广告" },
+              { value: 1548, name: "搜索引擎" }
+            ]
+          }
+        ]
+      };
+    }
 </script>
