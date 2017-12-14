53414d6e828326a84c9a8c351a4ce1d6f8fccc11
yokiyokiyoki <419703726@qq.com>
2017-12-14 20:48:35 +0800

    上传全局图表组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/page/Index'
+import Chart from '@/page/chart'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    path: '/',
-    name: 'index',
-    component: Index
-  }]
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Index
+    }, {
+      path: '/chart',
+      name: 'Chart',
+      component: Chart
+    }
+  ]
 })
