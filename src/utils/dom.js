c94bc15c26e142e7281cdcb7494ce9cdd4c0bd6f
yokiyokiyoki <419703726@qq.com>
2018-01-18 14:04:27 +0800

    dom的实用操作方法

---
+++ src/utils/dom.js
@@ -0,0 +1,22 @@
+// 一些dom的实用操作方法
+const dom = {
+  // 获取Y坐标的距离
+  getOffsetTop (dom) {
+    // 获取top偏移量
+    let top = dom.offsetTop
+    // 如果该元素的offsetParent存在，递归算出top
+    if (dom.offsetParent !== null) {
+      top += getOffsetTop(dom.offsetParent)
+    }
+    return top
+  },
+  // 获取X坐标的距离
+  getOffsetLeft (dom) {
+    let left = dom.offsetLeft
+    if (dom.offsetParent !== null) {
+      left += getOffsetLeft(dom.offsetParent)
+    }
+    return left
+  }
+}
+export default dom
