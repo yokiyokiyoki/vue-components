d0f0a8cfc10ccc730fd1116b1d66dded71289af7
yokiyokiyoki <419703726@qq.com>
2018-01-29 12:03:41 +0800

    新建一个date-table

---
+++ src/components/g-date-picker/src/base/date-table.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="picker-panel-content-cells">
+        <div class="cells-header">
+            <span v-for='day in headerDays' :key='day'>{{day}}</span>
+        </div>
+        <div class="cells-content">
+            <span :class="getCellCls(cell)" v-for='(cell,index) in cells' :key='index' @click='handleCell(cell)'>
+            <em>{{cell.day}}</em>
+            </span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      headerDays: ["日", "一", "二", "三", "四", "五", "六"],
+      cells: []
+    };
+  }
+};
+</script>
