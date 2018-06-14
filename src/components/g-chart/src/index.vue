05cd8ff83f338882931fdc844699da81f1543dcb
youjie <youjie@hudongpai.com>
2018-06-14 15:33:06 +0800

    添加一个容器元素

--- src/components/g-chart/src/index.vue
+++ src/components/g-chart/src/index.vue
@@ -1,85 +1,87 @@
 <template>
-    <div class="chart-box" ref='chart-box'></div>
+    <div class="chart-container">
+      <div class="chart-box" ref='chart-box'></div>
+    </div>
 </template>
 <script>
-import ec from "echarts";
-import "echarts-wordcloud";
-const R = require("ramda");
-import Vue from "vue";
-import "./index.less";
+import ec from 'echarts'
+import 'echarts-wordcloud'
+const R = require('ramda')
+import Vue from 'vue'
+import './index.less'
 // enumerating ECharts events 枚举echarts事件
 const ACTION_EVENTS = [
-  "legendselectchanged",
-  "legendselected",
-  "legendunselected",
-  "datazoom",
-  "datarangeselected",
-  "timelinechanged",
-  "timelineplaychanged",
-  "restore",
-  "dataviewchanged",
-  "magictypechanged",
-  "geoselectchanged",
-  "geoselected",
-  "geounselected",
-  "pieselectchanged",
-  "pieselected",
-  "pieunselected",
-  "mapselectchanged",
-  "mapselected",
-  "mapunselected",
-  "axisareaselected",
-  "brush",
-  "brushselected"
-];
+  'legendselectchanged',
+  'legendselected',
+  'legendunselected',
+  'datazoom',
+  'datarangeselected',
+  'timelinechanged',
+  'timelineplaychanged',
+  'restore',
+  'dataviewchanged',
+  'magictypechanged',
+  'geoselectchanged',
+  'geoselected',
+  'geounselected',
+  'pieselectchanged',
+  'pieselected',
+  'pieunselected',
+  'mapselectchanged',
+  'mapselected',
+  'mapunselected',
+  'axisareaselected',
+  'brush',
+  'brushselected'
+]
 
 const MOUSE_EVENTS = [
-  "click",
-  "dblclick",
-  "mouseover",
-  "mouseout",
-  "mousedown",
-  "mouseup",
-  "globalout"
-];
+  'click',
+  'dblclick',
+  'mouseover',
+  'mouseout',
+  'mousedown',
+  'mouseup',
+  'globalout'
+]
 export default {
-  name: "g-chart",
+  name: 'g-chart',
   data() {
     return {
       instance: null //echart实例
-    };
+    }
   },
   props: {
     dataComplete: {
       type: Boolean,
       default() {
-        return false;
+        return false
       },
       required: true
     },
     options: {
       type: Object,
       default() {
-        return {};
+        return {}
       },
       required: true
     },
     emptyText: {
       type: String,
       default() {
-        return "暂无数据";
+        return '暂无数据'
       }
     }
   },
   mounted() {
-    this.init();
+    this.init()
   },
   watch: {
     options: {
       handler: function(val, oldVal) {
         this.$nextTick(() => {
-          this.initOption();
-        });
+          this.initOption()
+        })
       },
       deep: true
     }
@@ -87,70 +89,71 @@ export default {
   methods: {
     init() {
       this.$nextTick(() => {
-        window.addEventListener("resize", this.resizeEventHandler, false);
-        this.initOption();
-      });
+        window.addEventListener('resize', this.resizeEventHandler, false)
+        this.initOption()
+      })
     },
     initOption() {
       if (this.dataComplete) {
         if (!R.isEmpty(this.options)) {
           if (this.options.series && this.options.series.length != 0) {
-            !this.instance && (this.instance = ec.init(this.$el, "default"));
-            this.instance.setOption(this.options, true);
+            !this.instance &&
+              (this.instance = ec.init(this.$refs['chart-box'], 'default'))
+            this.instance.setOption(this.options, true)
             // expose ECharts events as custom events（遍历echart事件emit）
             ACTION_EVENTS.forEach(event => {
               this.instance.on(event, params => {
-                this.$emit(event, params);
-              });
-            });
+                this.$emit(event, params)
+              })
+            })
             MOUSE_EVENTS.forEach(event => {
               this.instance.on(event, params => {
-                this.$emit(event, params);
-              });
-            });
+                this.$emit(event, params)
+              })
+            })
           } else {
             //series没有的时候则销毁该实例
-            this.instance && this.instance.dispose();
-            this.instance = null;
-            this.noDataOption();
+            this.instance && this.instance.dispose()
+            this.instance = null
+            this.noDataOption()
           }
         } else {
-          this.noDataOption();
+          this.noDataOption()
         }
       }
     },
     resizeEventHandler() {
       if (this.instance) {
-        throttle(this.instance.resize(), 300);
+        throttle(this.instance.resize(), 300)
       }
     },
     noDataOption() {
       //如果node存在子节点则先删除
-      while (this.$refs["chart-box"].firstChild) {
-        this.$refs["chart-box"].removeChild(this.$refs["chart-box"].firstChild);
+      while (this.$refs['chart-box'].firstChild) {
+        this.$refs['chart-box'].removeChild(this.$refs['chart-box'].firstChild)
       }
       let emptyComponent = Vue.extend({
-        template: "<div class='empty'><span>" + this.emptyText + "</span></div>"
-      });
-      let empty = new emptyComponent().$mount();
-      this.$refs["chart-box"].appendChild(empty.$el);
+        template: "<div class='empty'><span>" + this.emptyText + '</span></div>'
+      })
+      let empty = new emptyComponent().$mount()
+      this.$refs['chart-box'].appendChild(empty.$el)
     }
   },
   beforeDestroy() {
-    window.addEventListener("resize", this.resizeEventHandler, false);
+    window.addEventListener('resize', this.resizeEventHandler, false)
   }
-};
+}
 
 // 节流函数
 function throttle(fn, delay) {
-  var timer = null;
+  var timer = null
   return function() {
     var context = this,
-      args = arguments;
-    clearTimeout(timer);
+      args = arguments
+    clearTimeout(timer)
     timer = setTimeout(function() {
-      fn.apply(context, args);
-    }, delay);
-  };
+      fn.apply(context, args)
+    }, delay)
+  }
 }
 </script>
