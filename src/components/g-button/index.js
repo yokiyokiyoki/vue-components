ccbd097609963a80f93d4b8c417df0ce8edd362d
youjie <youjie@hudongpai.com>
2018-09-26 15:07:28 +0800

    feat(组件): 准备按钮

---
+++ src/components/g-button/index.js
@@ -0,0 +1,2 @@
+import Button from './src/index'
+export default Button
---
+++ src/components/g-button/src/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="g-button">
+        
+    </div>
+</template>
+<script>
+export default {}
+</script>
