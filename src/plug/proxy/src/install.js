f8c8f43feb46c7b00dd274425acca54fbba206ac
youjie <youjie@hudongpai.com>
2018-09-26 11:40:25 +0800

    feat(ajax通用函数): 增加请求拦截器和响应拦截器和基础配置

--- src/plug/proxy/src/install.js
+++ src/plug/proxy/src/install.js
@@ -11,70 +11,87 @@ const install = function (Vue, option = {}) {
   if (install.installed) return
   install.installed = true
   Util(Vue)
+  // 基础配置
+  axios.defaults.baseURL = '/'
+  // 设置默认请求头
+  axios.defaults.headers = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  axios.defaults.timeout = 100000
+  // 请求拦截器
+  axios.interceptors.request.use(
+    config => {
+      if (!config.ajaxOpt.noAbort) {
+        // 如果它是不被abort的，才加进map
+        // 发起请求时，取消掉当前正在进行的相同请求
+        if (Vue.ajaxMap[`${config.url}`]) {
+          Vue.ajaxMap[`${config.url}`](`${config.url}重复发请求,操作取消`)
+        }
+        Vue.ajaxMap[`${config.url}`] = cancel[`${config.url}`]
+      }
+
+      if (config.method === 'post') {
+        config.transformRequest = [
+          function (data) {
+            // 这里可以在发送请求之前对请求数据做处理，比如form-data格式化等
+            data = Qs.stringify(data)
+            return data
+          }
+        ]
+      }
+      return config
+    },
+    error => {
+      return Promise.reject(error)
+    }
+  )
+
+  // 响应拦截器
+  axios.interceptors.response.use(
+    config => {
+      let params = config.params ? JSON.stringify(config.params) : ''
+      // 自定义状态，如果超时或者什么的，返回到首页/登录页
+      let data = config.data
+      if (data.code == 1000) {
+        // debugger;
+        Vue.prototype.$abort()
+        // 还要清掉localstorage
+        localStorage.removeItem('userInfo')
+        location.href = '/login'
+      }
+      // 删掉ajaxMap里面的
+      deleteAjaxMap(Vue, `${config.url}${params}`)
+      return config
+    },
+    err => {
+      return Promise.reject(err)
+    }
+  )
   Vue.prototype.$proxy = (ajaxParams = {}, ajaxOpt) => {
-    // 根据url和data生成唯一key
-    const ajaxKey = `${ajaxParams.url}`
     ajaxOpt = Object.assign(
       {
         mock: false, // 默认不开启mock数据
-        noAbort: false // 默认切换页面会abort请求，可以指定一些不abort掉请求的
+        noAbort: false // 不能被规则abort
       },
       ajaxOpt
     )
     if (ajaxOpt.mock) {
       ajaxParams.url = `/mock${ajaxParams.url}`
     }
-    if (ajaxParams.method == 'post') {
-      // 如果是post请求，需要修改请求头,令其提交为from-data
-      ajaxParams.headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-      ajaxParams.transformRequest = [
-        function (data) {
-          // 这里可以在发送请求之前对请求数据做处理，比如form-data格式化等
-          data = Qs.stringify(data)
-          return data
-        }
-      ]
-    }
-    // 默认参数
-    ajaxParams = Object.assign(
-      {
-        // abort掉请求
-        cancelToken: new CancelToken(function executor (c) {
-          // An executor function receives a cancel function as a parameter
-          setAjaxMap(Vue, ajaxKey, c)
-        })
-      },
-      ajaxParams
-    )
-    // all responses interceptors
-    instance.interceptors.response.use(
-      config => {
-        // 自定义状态，如果超时或者什么的，返回到首页/登录页
-        let data = config.data
-        if (data.success && data.code == 1000) {
-          router.push('/Index')
-        }
-        return config
-      },
-      err => {
-        return Promise.reject(err)
-      }
-    )
-    return instance(ajaxParams)
-      .then(res => {
-        delAjaxMap(Vue, ajaxKey)
-        return res.data
-      })
-      .catch(err => {
-        delAjaxMap(Vue, ajaxKey)
-        return err
-      })
+
+    // 返回请求
+    return axios({
+      ...ajaxParams,
+      cancelToken: new CancelToken(c => {
+        cancel[`${ajaxParams.url}`] = c
+      }),
+      ajaxOpt
+    })
   }
   Vue.abort = Vue.prototype.$abort = () => {
     for (let key in Vue.ajaxMap) {
-      Vue.ajaxMap[key]()
+      Vue.ajaxMap[key]('切换路由，操作取消')
       delAjaxMap(Vue, key)
     }
   }
